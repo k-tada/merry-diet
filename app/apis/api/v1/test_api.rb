2832d18e953ae8949dc30c0673dfa9dbc1bb0eec
k.tada <white.rv0925@gmail.com>
2015-09-28 03:52:11 +0000

    #8 Grape::Entityのベース作成

--- app/apis/api/v1/test_api.rb
+++ app/apis/api/v1/test_api.rb
@@ -4,7 +4,11 @@ module API
       resource :test do
         desc 'GET /api/v1/test'
         get '/' do
-          {message: :ok}
+          ret = {
+            user: 'Tom',
+            message: 'ok'
+          }
+          present ret, with: API::V1::Entities::TestEntity
         end
       end
     end
