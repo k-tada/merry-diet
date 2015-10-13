e6dd5b764c94c6b9d848e52189576beda8e94078
k.tada <white.rv0925@gmail.com>
2015-10-13 13:08:08 +0000

    ユーザマスタにuuidを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151009041707) do
+ActiveRecord::Schema.define(version: 20151013130908) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,10 +32,12 @@ ActiveRecord::Schema.define(version: 20151009041707) do
     t.string   "token"
     t.string   "space_id"
     t.string   "phone_number"
+    t.string   "uuid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+  add_index "users", ["uuid"], name: "index_users_on_uuid", unique: true
 
 end
